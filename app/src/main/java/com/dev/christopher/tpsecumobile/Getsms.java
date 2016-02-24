23dc004f6c8307afe79d9a0c055db2cf1e334578
Christopher <christopher.manouana.sio@gmail.com>
2016-02-24 23:30:55 +0100

    first commit

---
+++ app/src/main/java/com/dev/christopher/tpsecumobile/Getsms.java
@@ -0,0 +1,43 @@
+package com.dev.christopher.tpsecumobile;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Christopher on 09/12/2015.
+ */
+public class Getsms extends BroadcastReceiver {
+    final SmsManager sms = SmsManager.getDefault();
+
+    public static final String SMS_BUNDLE = "pdus";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle intentExtras = intent.getExtras();
+        if (intentExtras != null) {
+            Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
+            String smsMessageStr = "";
+            for (int i = 0; i < sms.length; ++i) {
+                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+
+                String smsBody = smsMessage.getMessageBody().toString();
+                String address = smsMessage.getOriginatingAddress();
+
+                smsMessageStr += "SMS From: " + address + "\n";
+                smsMessageStr += smsBody + "\n";
+            }
+            Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
+            Bundle bundle = intent.getExtras();
+            MainActivity mainActivity = new MainActivity();
+            MainActivity.getInstance().updateTextView(smsMessageStr);
+
+
+
+        }
+    }
+}
